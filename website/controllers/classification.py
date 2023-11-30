6a2fa938608a88500914c2867882db2c1bfe36e9
Muhammad Rifki <mrifki028@gmail.com>
2023-11-30 11:09:13 +0700

    fix classificcation

--- website/controllers/classification.py
+++ website/controllers/classification.py
@@ -1,16 +1,14 @@
+from math import nan
 from sklearn.calibration import LabelEncoder
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix
 from website.models import Models
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import LabelBinarizer
 from flask import flash, json
 import pickle
 import os
+import numpy as np
 
 from website.multinb import MultiNB
-
 
 class ClassificationController:
     def createModel(self):
@@ -28,7 +26,7 @@ class ClassificationController:
         list_text_training = []
         list_label_training = []
 
-        instance_model = Models('SELECT clean_text, sentiment FROM tbl_data_train_q5')
+        instance_model = Models('SELECT clean_text, sentiment FROM tbl_data_train_q4')
         data_train = instance_model.select()
 
         for i in range(len(data_train)):
@@ -39,7 +37,7 @@ class ClassificationController:
         list_text_testing = []
         list_label_testing = []
 
-        instance_model = Models('SELECT clean_text, sentiment FROM tbl_data_test_q5')
+        instance_model = Models('SELECT clean_text, sentiment FROM tbl_data_test_q4')
         data_test = instance_model.select()
         for i in range(len(data_test)):
             list_text_testing.append(data_test[i]['clean_text'])
@@ -58,15 +56,8 @@ class ClassificationController:
 
         label = labelEncoder.classes_
 
-        print(X_train.shape)
-        print(X_test.shape)
-        print(y_train.shape)
-        print(y_test.shape)
-        # print(vocab)
-        print(label)
-
         # TRAIN MULTINOMIAL NAIVE BAYES
-        model = MultinomialNB()
+        model = MultiNB()
         model.fit(X_train, y_train)
 
         # SAVE MODEL as PKL
@@ -92,39 +83,71 @@ class ClassificationController:
         # Mengambil hasil probabilitas prediksi label
         list_prob_prediksi = []
 
-        predict_prob = model.predict_proba(X_test)
+        predict_prob = model.predict_proba
         for i in range(len(predict_prob)):
-            if predict_prob[i][0] > predict_prob[i][1]:
-                list_prob_prediksi.append(predict_prob[i][0])
-            else:
-                list_prob_prediksi.append(predict_prob[i][1])
+            tuple_pred = (round(float(predict_prob[i][0]), 3), round(float(predict_prob[i][1]), 3), round(float(predict_prob[i][2]), 3))
+            list_prob_prediksi.append(tuple_pred)
+        conf = confusion_matrix(y_test, y_pred)
+        print(conf)
+        TKurangPuas, FKurangPuas1, FKurangPuas2, FPuas1, TPuas, FPuas2, FSangatPuas1, FSangatPuas2, TSangatPuas = confusion_matrix(y_test, y_pred).ravel()
 
-        list_label_pred = labelEncoder.inverse_transform(y_pred)
+        akurasi = (TKurangPuas + TPuas + TSangatPuas) / (TKurangPuas + FKurangPuas1 + FKurangPuas2 + TPuas + FPuas1 + FPuas2 + FSangatPuas1 + FSangatPuas2 + TSangatPuas)
 
-        conf_matrix = confusion_matrix(y_test, y_pred)
-        print(conf_matrix.ravel())
-        print(conf_matrix)
+        if ((TKurangPuas + FPuas1 + FSangatPuas1) != 0):
+            presisi_negatif = TKurangPuas / (TKurangPuas + FPuas1 + FSangatPuas1)
+        else: 
+            presisi_negatif = 0
+        
+        if ((TPuas + FKurangPuas1 + FSangatPuas2)):
+            presisi_netral = TPuas / (TPuas + FKurangPuas1 + FSangatPuas2)
+        else:
+            presisi_netral = 0 
 
-        accuracy = accuracy_score(y_test, y_pred)
-        report = classification_report(y_test, y_pred)
-        print(f"Accuracy: {accuracy}")
-        print("Classification Report:\n", report)
+        if ((TSangatPuas + FKurangPuas2 + FPuas2) != 0):
+            presisi_positif = TSangatPuas / (TSangatPuas + FKurangPuas2 + FPuas2)
+        else:
+            presisi_positif = 0
 
-        true_neg, false_pos, false_neg, true_pos = confusion_matrix(y_test, y_pred).ravel()
+        presisi = (presisi_negatif + presisi_netral + presisi_positif) / len(label)
 
-        akurasi = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-        presisi = true_pos / (true_pos + false_pos)
-        recall = true_pos / (true_pos + false_neg)
+        if ((TKurangPuas + FKurangPuas1 + FKurangPuas1) != 0):
+            recall_negatif = TKurangPuas / (TKurangPuas + FKurangPuas1 + FKurangPuas1)
+        else:
+            recall_negatif = 0
+
+        if ((TPuas + FPuas1 + FPuas2)):
+            recall_netral = TPuas / (TPuas + FPuas1 + FPuas2)
+        else:
+            recall_netral = 0
+
+        if ((TSangatPuas + FSangatPuas1 + FSangatPuas2)):
+            recall_positif = TSangatPuas / (TSangatPuas + FSangatPuas1 + FSangatPuas2) 
+        else:
+            recall_positif = 0
+
+        recall = (recall_negatif + recall_netral + recall_positif) / len(label)
 
         data_dict = {
             "text_list" : list_text_testing,
             "label_list" : list_label_testing,
             "predict_label" : list_label_prediksi,
-            "prob_predict" : list_prob_prediksi,
-            "tp" : int(true_pos),
-            "tn" : int(true_neg),
-            "fp" : int(false_pos),
-            "fn" : int(false_neg),
+            "predict_prob" : list_prob_prediksi,
+            "tneg" : int(TKurangPuas),
+            "fneg1" : int(FKurangPuas1),
+            "fneg2" : int(FKurangPuas2),
+            "fnet1" : int(FPuas1),
+            "tnet" : int(TPuas),
+            "fnet2" : int(FPuas2),
+            "fpos1" : int(FSangatPuas1),
+            "fpos2" : int(FSangatPuas2),
+            "tpos" : int(TSangatPuas),
+            "jumlah_kelas": int(len(label)),
+            "presisi_negatif" : round(float(presisi_negatif), 2),
+            "presisi_netral" : round(float(presisi_netral), 2),
+            "presisi_positif" : round(float(presisi_positif), 2),
+            "recall_negatif" : round(float(recall_negatif), 2),
+            "recall_netral" : round(float(recall_netral), 2),
+            "recall_positif" : round(float(recall_positif), 2),
             "akurasi" : round(float(akurasi), 2),
             "presisi" : round(float(presisi), 2),
             "recall" : round(float(recall), 2)
